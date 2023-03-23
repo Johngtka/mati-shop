030f6a501ba437b167250d757d6c9a4f23dc0fa4
Johngtka <johngtka@gmail.com>
2023-03-23 14:21:50 +0100

    feat: making layout to form

--- konto.php
+++ konto.php
@@ -8,13 +8,14 @@
 
 <body style="margin: 0">
     <section style="width: 100%; height: 100vh; text-align:center">
-        <main style="padding: 30px; width: 50%; margin-left: auto; margin-right: auto; border: 2px solid #000; border-radius: 1pc">
+        <main style="padding: 30px; width: 20%; margin-left: auto; margin-right: auto; border: 2px solid #000; border-radius: 1pc">
             <form action="#" method="post">
                 <label>Login: <input type="text" name="login"></label><br>
                 <label>Hasło: <input type="password" name="haslo"></label><br>
                 <input type="reset" value="Wyszyść">
                 <input type="submit" value="Zaloguj">
             </form>
+            <br><a href="#" style="font-family: arial; text-decoration: none; font-weight:800; color:#fff; padding: 10px; background-color:orange; border-radius: 5px; font-size:15px">Zarejestruj</a>
         </main>
     </section>
 </body>
