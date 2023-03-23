41d0c8b0ec5e0b7d5148025b41249e2e86d52746
Johngtka <johngtka@gmail.com>
2023-03-23 14:06:06 +0100

    feat: logging and register system (in process)

--- konto.php
+++ konto.php
@@ -1,2 +1,22 @@
-Nie masz konta? 
-Wkrótce pojawi się możliwość rejstracji na naszej stronie internetowej
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Elektronika dla Ciebie</title>
+</head>
+
+<body style="margin: 0">
+    <section style="width: 100%; height: 100vh; text-align:center">
+        <main style="padding: 30px; width: 50%; margin-left: auto; margin-right: auto; border: 2px solid #000; border-radius: 1pc">
+            <form action="#" method="post">
+                <label>Login: <input type="text" name="login"></label><br>
+                <label>Hasło: <input type="password" name="haslo"></label><br>
+                <input type="reset" value="Wyszyść">
+                <input type="submit" value="Zaloguj">
+            </form>
+        </main>
+    </section>
+</body>
+
+</html>
